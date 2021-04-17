cde06a44d85047f73f428c89151bed1c38cc30a0
victornado <victorvelascoarjona@gmail.com>
2021-04-17 13:03:54 +0200

    main comments

--- practica 1/src/Main/MAIN.java
+++ practica 1/src/Main/MAIN.java
@@ -5,40 +5,9 @@ import Controlador.Controller;
 public class MAIN {
 
 	public static void main(String[] args) {
-//		AlgoritmoA game = new AlgoritmoA(4, 4);
-//		BufferedReader entrada;
-//		String cadena;
-		Controller c =new Controller();
-		try {
-//			entrada = new BufferedReader(new InputStreamReader(System.in));
-//			System.out.print("componente i de inicio:");
-//			//cadena = entrada.readLine();
-//			//int i = Integer.parseInt(cadena);
-//			int i=0;
-//			System.out.print("componente j de inicio:");
-//			//cadena = entrada.readLine();
-//			int j=0;
-//			//int j = Integer.parseInt(cadena);
-//			game.crearInicio(i, j);
-//			System.out.print("componente i de fin:");
-//			//cadena = entrada.readLine();
-//			i=3;
-//			//i = Integer.parseInt(cadena);
-//			System.out.print("componente j de fin:");
-//			//cadena = entrada.readLine();
-//			j=3;
-//			//j = Integer.parseInt(cadena);
-//			game.crearFin(i, j);
-//			game.meterObstaculos(1, 1);
-//			game.meterObstaculos(0, 1);
-//			game.meterObstaculos(1, 0);
-//			game.generarSolucion();
-//			game.sol();
 
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Controller c =new Controller();
+
 	}
 
 }
